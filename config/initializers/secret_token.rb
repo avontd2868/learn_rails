6e05f46d39f91040d7b366b2e3cc25f6c1136d17
root <root@FedoraHost.FedoraDomain>
2013-07-13 18:21:40 +0530

    Started with hello world

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '525ee2d2f54b93b3c7313150af07ed18a58e1867c29df1aa236e26506269c787ab4fafbe1a554fe81456339dd9fdf84a7826b20e19a0ceef0c07bd12d7811a0c'
